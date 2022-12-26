7fb41aa9654aa7396f0cf46f285014f6a0c2286a
ag <>
2022-12-26 00:50:53 +0000

    Make commands enumeration

---
+++ vmtools/_commands.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Command(Enum):
+    ARITHMETIC = 0
+    PUSH = 1
+    POP = 2
+    LABEL = 3
+    GOTO = 4
+    IF = 5
+    FUNCTION = 6
+    RETURN = 7
+    CALL = 8
